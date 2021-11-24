ed1479299a36c46eb1533c59e5d62d9645bc9800
RichterElric <67111728+RichterElric@users.noreply.github.com>
2021-11-24 16:16:52 +0100

    install scripts v 0.0.1

---
+++ install.ps1
@@ -0,0 +1,35 @@
+# Create install directory
+
+# Check if python exist
+Write-Host "Make sure python 3 and pip is installed on your system"
+
+if (-not (Test-Path "$PSScriptRoot/bin"))
+{
+    New-Item -ItemType Directory -Path "$PSScriptRoot/bin" | Out-Null
+}
+# Install kics
+if (-not [System.IO.File]::Exists("$PSScriptRoot/bin/kics.exe"))
+{
+    Invoke-WebRequest "https://github.com/Checkmarx/kics/releases/download/v1.4.7/kics_1.4.7_windows_x64.zip" -O "$PSScriptRoot/bin/kics.zip"
+    Expand-Archive "$PSScriptRoot/bin/kics.zip" -DestinationPath "$PSScriptRoot/bin/"
+    Remove-Item -Path "$PSScriptRoot/bin/kics.zip"
+}
+# Install git-secret
+if (-not (Test-Path "$PSScriptRoot/bin/gitsecret"))
+{
+    Invoke-WebRequest "https://github.com/awslabs/git-secrets/archive/refs/heads/master.zip" -O "$PSScriptRoot/bin/gitsecret.zip"
+    Expand-Archive "$PSScriptRoot/bin/gitsecret.zip" -DestinationPath "$PSScriptRoot/bin/gitsecret"
+    Get-ChildItem -Path "$PSScriptRoot/bin/gitsecret/git-secrets-master" -Recurse -File | Move-Item -Destination "$PSScriptRoot/bin/gitsecret/"
+    Remove-Item -Path "$PSScriptRoot/bin/gitsecret.zip"
+    & "$PSScriptRoot/bin/gitsecret/install.ps1"
+}
+# Install Keyfinder
+if (-not -(Test-Path "$PSScriptRoot/bin/keyfinder"))
+{
+    Invoke-WebRequest "https://github.com/CERTCC/keyfinder/archive/refs/heads/master.zip" -O "$PSScriptRoot/bin/keyfinder.zip"
+    Expand-Archive "$PSScriptRoot/bin/keyfinder.zip" -DestinationPath "$PSScriptRoot/bin/keyfinder"
+    Get-ChildItem -Path "$PSScriptRoot/bin/keyfinder/keyfinder-master" -Recurse -File | Move-Item -Destination "$PSScriptRoot/bin/keyfinder/"
+    Remove-Item -Path "$PSScriptRoot/bin/keyfinder.zip"
+    Remove-Item -Path "$PSScriptRoot/bin/keyfinder/keyfinder-master"
+    Invoke-Item (start powershell "pip install install androguard python-magic-bin PyOpenSSL")
+}
---
+++ install.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+if [ "$EUID" -ne 0 ]
+then
+  echo "Please run as root"
+  exit
+fi
+
+echo $SCRIPT_DIR
+### Install utilities ###
+# Check if go is installed
+if ! command -v go &> /dev/null
+then
+  echo "go not found, downloading"
+  apt-get install go
+fi
+# Check if python is installed
+if ! command -v python3 &> /dev/null
+then
+  echo "python3 not found, downloading"
+  apt-get install python3
+fi
+# Installing pip
+if ! command -v pip3 &> /dev/null
+then
+  echo "pip3 not found, downloading"
+  apt-get install pip3
+fi
+# tar not found
+if ! command -v gunzip &> /dev/null
+then
+  echo "gunzip not found, downloading"
+  apt-get install gunzip
+fi
+# wget not found
+if ! command -v wget &> /dev/null
+then
+  echo "wget not found, downloading"
+  apt-get install wget
+fi
+# git not found
+if ! command -v git &> /dev/null
+then
+  echo "git not found, downloading"
+  apt-get install git
+fi
+
+### utilities end ###
+
+# bin init
+if ! [[ -d "./bin" ]]
+then
+  mkdir "$SCRIPT_DIR/bin"
+fi
+
+# Install kics
+if ! [[ -f "$SCRIPT_DIR/bin/kics" ]]
+then
+  wget https://github.com/Checkmarx/kics/releases/download/v1.4.7/kics_1.4.7_linux_x64.tar.gz -O "$SCRIPT_DIR/bin/kics.tar.gz"
+  tar -xf "$SCRIPT_DIR/bin/kics.tar.gz" -C "$SCRIPT_DIR/bin"
+  rm "$SCRIPT_DIR/bin/kics.tar.gz"
+fi
+
+# Install keyfinder
+pip3 install -r ./requirements.txt -q
+if ! [[ -d "$SCRIPT_DIR/bin/keyfinder" ]]
+then
+  git clone https://github.com/CERTCC/keyfinder.git "$SCRIPT_DIR/bin/keyfinder/"
+fi
+
+# Install gitsecret
+if ! [[ -d "$SCRIPT_DIR/bin/gitsecret" ]]
+then
+  git clone https://github.com/awslabs/git-secrets.git $SCRIPT_DIR/bin/gitsecret/
+fi
+sleep 1
+cd "$SCRIPT_DIR/bin/gitsecret/" && make install
+
+# Build our app
+go build $SCRIPT_DIR
